693a73e0ac07da1ed156a4bd5f3efea6df463ac0
nasanchez7 <nadirblanco02@gmail.com>
2024-04-02 11:59:54 -0300

    proyecto terminado

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ window.addEventListener('load', () => {
     const buttonClose = document.getElementById('buttonClose')
     if(buttonMenu){
         const menuSecundario = document.getElementById('menuSecundario')
+        const nav = document.getElementById('nav')
         if(menuSecundario){
             buttonMenu.addEventListener('click', () => {
                 menuSecundario.classList.add('activeMenu')
                 buttonMenu.classList.add('d-none')
                 buttonClose.classList.remove('d-none')
+                nav.classList.remove('navSombra')
+                nav.style.borderBottom = "1px solid lightgray"
             })
             buttonClose.addEventListener('click', () => {
                 menuSecundario.classList.remove('activeMenu')
                 buttonMenu.classList.remove('d-none')
                 buttonClose.classList.add('d-none')
+                nav.classList.add('navSombra')
+                nav.style.borderBottom = "0px solid #707070"
             })
         }
     }
